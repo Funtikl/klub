b0c59084aa8ef2248b1aa8f2e585808e4651cb39
Fuad Rustamzadeh <rustam.fuad@gmail.com>
2019-01-12 16:24:24 +0400

    env setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+//modules
+import cssStyles from './First.module.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
