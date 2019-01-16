dd33c29670056a5973eacc0b0bb01a2b4bbc10f2
Fuad Rustamzadeh <rustam.fuad@gmail.com>
2019-01-16 13:18:56 +0400

    lastdonates css

--- src/components /homepage/lastdonates.js
+++ src/components /homepage/lastdonates.js
@@ -6,9 +6,13 @@ import {
   CardTitle,
   CardText,
   Row,
-  Col
+  Col,
+  CardLink
 } from "reactstrap";
 import homestyles from "./home.module.css";
+
+import image1 from './images/img_1.jpg';
+import image2 from './images/img_2.jpg';
 export default class LastDonat extends Component {
   render() {
     return (
@@ -16,27 +20,27 @@ export default class LastDonat extends Component {
         <h2 className={homestyles.lastDonatH2}>Last Donations</h2>
         <Row>
           <Col sm="6">
-            <Card body>
+            <Card className={homestyles.lastDonatCard} body>
               <CardTitle>Special Title Treatment</CardTitle>
-              <CardImg />
+              <CardImg src={image1} />
               <CardText>
                 With supporting text below as a natural lead-in to additional
                 content.
               </CardText>
-              <Button>Go somewhere</Button>
-              <Button>Go somewhere</Button>
+              <CardLink className={homestyles.lastDonateButton1} href="#">Card Link</CardLink>
+              <CardLink className={homestyles.lastDonateButton2} href="#">Another Link</CardLink>
             </Card>
           </Col>
           <Col sm="6">
-            <Card body>
+            <Card className={homestyles.lastDonatCard} body>
               <CardTitle>Special Title Treatment</CardTitle>
-              <CardImg />
+              <CardImg src={image2}/>
               <CardText>
                 With supporting text below as a natural lead-in to additional
                 content.
               </CardText>
-              <Button>Go somewhere</Button>
-              <Button>Go somewhere</Button>
+              <CardLink className={homestyles.lastDonateButton1} href="#">Card Link</CardLink>
+              <CardLink className={homestyles.lastDonateButton2} href="#">Another Link</CardLink>
             </Card>
           </Col>
         </Row>
--- src/components /homepage/top.js
+++ src/components /homepage/top.js
@@ -13,15 +13,19 @@ import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
+import image1 from './images/img_1.jpg';
+import image2 from './images/img_2.jpg';
+import image3 from './images/img_3.jpg';
 export default class Top extends Component {
   render() {
     return (
       <div className={homestyles.topCardsDiv}>
         <h2 className={homestyles.topH2}>Top Donations</h2>
         <Row>
-          <OwlCarousel  className="owl-theme"  loop items={3} margin={10}>
-            <Col className={homestyles.topCol} class='item' sm="10">
+          <OwlCarousel autoplay={true}  className="owl-theme"  loop items={3} margin={10}>
+            <Col className={homestyles.topCol} class='item' sm="12">
               <Card  body>
+              <CardImg src={image1} />
                 <CardTitle>Special Title Treatment</CardTitle>
                 <CardText>
                   With supporting text below as a natural lead-in to additional
@@ -30,8 +34,9 @@ export default class Top extends Component {
                 <Button>Go somewhere</Button>
               </Card>
             </Col>
-            <Col className={homestyles.topCol} class="item" sm="10">
+            <Col className={homestyles.topCol} class="item" sm="12">
               <Card  body>
+              <CardImg src={image2} />
                 <CardTitle>Special Title Treatment</CardTitle>
                 <CardText>
                   With supporting text below as a natural lead-in to additional
@@ -40,6 +45,17 @@ export default class Top extends Component {
                 <Button>Go somewhere</Button>
               </Card>
             </Col>
+            <Col className={homestyles.topCol} class="item" sm="12">
+            <Card  body>
+            <CardImg src={image3} />
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </CardText>
+              <Button>Go somewhere</Button>
+            </Card>
+          </Col>
           </OwlCarousel>
         </Row>
       </div>
